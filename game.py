1500a2cc51e787c4aab9b72f8fe2a8f168bff09e
Sravani-1309 <142100126+Sravani-1309@users.noreply.github.com>
2023-08-12 20:18:08 +0530

    Create game.py

---
+++ game.py
@@ -0,0 +1,15 @@
+from random import *
+print("1.Rock\n2.Paper\n3.Scissor")
+list=[1,2,3]
+a=int(input("Choose your option"))
+if a not in list:
+    print("Enter valid option:")
+else:
+    b=choice(list)
+    print(b)
+    if (a==b):
+        print("Match is tie")
+    elif (a==1 and b==2) or (a==3 and b==1) or (a==2 and b==3):
+        print("You lose the match")
+    else:
+        print("You won the match")
